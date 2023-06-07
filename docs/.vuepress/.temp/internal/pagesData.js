846726023c0a95e512c448b4b52d9744b12eb4de
ll <etatata@gmail.com>
2023-06-08 00:53:02 +0800

    dd

---
+++ docs/.vuepress/.temp/internal/pagesData.js
@@ -0,0 +1,10 @@
+export const pagesData = {
+  // path: /404.html
+  "v-3706649a": () => import(/* webpackChunkName: "v-3706649a" */"/Users/ll/gaga/olderCodeFarmer/docs/.vuepress/.temp/pages/404.html.js").then(({ data }) => data),
+  // path: /guide/getting-started.html
+  "v-fb0f0066": () => import(/* webpackChunkName: "v-fb0f0066" */"/Users/ll/gaga/olderCodeFarmer/docs/.vuepress/.temp/pages/guide/getting-started.html.js").then(({ data }) => data),
+  // path: /contributing.html
+  "v-6ce48554": () => import(/* webpackChunkName: "v-6ce48554" */"/Users/ll/gaga/olderCodeFarmer/docs/.vuepress/.temp/pages/contributing.html.js").then(({ data }) => data),
+  // path: /
+  "v-8daa1a0e": () => import(/* webpackChunkName: "v-8daa1a0e" */"/Users/ll/gaga/olderCodeFarmer/docs/.vuepress/.temp/pages/index.html.js").then(({ data }) => data),
+}
---
+++ docs/.vuepress/.temp/internal/pagesRoutes.js
@@ -0,0 +1,6 @@
+export const pagesRoutes = [
+  ["v-3706649a","/404.html",{"title":""},["/404"]],
+  ["v-fb0f0066","/guide/getting-started.html",{"title":""},["/guide/getting-started","/guide/getting-started.md"]],
+  ["v-6ce48554","/contributing.html",{"title":""},["/contributing","/contributing.md"]],
+  ["v-8daa1a0e","/",{"title":"Title of this page"},["/index.html","/README.md"]],
+]
